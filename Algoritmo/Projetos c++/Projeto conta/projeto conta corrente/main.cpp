dca49153297d5fdf9ead5cdbb5158ab8ce283643
VALDELAINE CRISTINA RIBEIRO <valdelaine.ribeiro4@gmail.com>
2022-06-20 15:08:50 -0300

    Add files via upload

---
+++ Algoritmo/Projetos c++/Projeto conta/projeto conta corrente/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+//Faça um algoritmo em c++ para o controle de uma conta corrente.
+//O algoritmo deve apresentar um menu com as seguintes opções:
+//1-deposito
+//2-retirada
+//3-saldo
+//4-sair
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
+	return 0;
+}
